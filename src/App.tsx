8fa81e1736fc1e98dabdaeeeae767c82ffc737cb
Adam Ranieri <adaman94@gmail.com>
2022-01-25 15:59:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import LoginPage from './components/login-page';
 import EmployeeHomePage from './components/employee-home-page';
 import MemberHomePage from './components/member-home-page';
